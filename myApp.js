b0a064c6e1844a9930812005438ccc4c9f0058e0
moreen isaac <nkirotemoreen70@gmail.com>
2021-03-26 04:00:57 +0300

    getting started with backend

--- myApp.js
+++ myApp.js
@@ -1,39 +1,7 @@
 var express = require('express');
 var app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/views/index.html");
+})
+app.use("/public", express.static(__dirname + "/public"))
